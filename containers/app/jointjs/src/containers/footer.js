372dadd7846b93b5c0d2f65135d375f63c263591
wvanheemstra <wvanheemstra@users.noreply.github.com>
2023-06-16 16:46:25 +0200

    Create footer.js

---
+++ containers/app/jointjs/src/containers/footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer className="page-footer center-align light-blue darken-4">
+            <div className="footer-copyright">
+                <div className="container center-align">
+                    Copyright &copy; 2023. All Rights Reserved
+                </div>
+            </div>
+      </footer>
+
+    );
+  }
+}
+
+export default Footer;
