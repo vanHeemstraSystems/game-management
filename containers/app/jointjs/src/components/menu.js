256d961c8839f7510e8eb0d61a7d395509cc0f5a
wvanheemstra <wvanheemstra@users.noreply.github.com>
2023-06-16 16:43:36 +0200

    Update menu.js

--- containers/app/jointjs/src/components/menu.js
+++ containers/app/jointjs/src/components/menu.js
@@ -6,48 +6,48 @@ class Menu extends React.Component {
         super(props);
         this.state = {
             data: [
-              {
-                  "id": 1,
-                  "componentName": "Value-Streams",
-                  "color": "blue",
-                  "position": {"x": 36, "y": 36}
-              },
-              {
-                  "id": 2,
-                  "componentName": "Jobs-to-be-Done",
-                  "color": "orange",
-                  "position": {"x": 40, "y": 40}
-              },
-              {
-                  "id": 3,
-                  "componentName": "Problem",
-                  "color": "red",
-                  "position": {"x": 44, "y": 44}
-              },
-              {
-                  "id": 4,
-                  "componentName": "Solution",
-                  "color": "green",
-                  "position": {"x": 48, "y": 48}
-              },
-              {
-                  "id": 5,
-                  "componentName": "Goal",
-                  "color": "olive",
-                  "position": {"x": 52, "y": 52}
-              },
-              {
-                  "id": 6,
-                  "componentName": "Process",
-                  "color": "lime",
-                  "position": {"x": 56, "y": 56}
-              },
-              {
-                  "id": 7,
-                  "componentName": "Action",
-                  "color": "lightblue",
-                  "position": {"x": 60, "y": 60}
-              }
+                            {
+                                "id": 1,
+                                "componentName": "Company Purpose",
+                                "color": "red",
+                                "position": {"x": 36, "y": 36}
+                            },
+                            {
+                                "id": 2,
+                                "componentName": "Portfolio",
+                                "color": "orange",
+                                "position": {"x": 40, "y": 40}
+                            },
+                            {
+                                "id": 3,
+                                "componentName": "Product Portfolio",
+                                "color": "yellow",
+                                "position": {"x": 44, "y": 44}
+                            },
+                            {
+                                "id": 4,
+                                "componentName": "Goals",
+                                "color": "green",
+                                "position": {"x": 48, "y": 48}
+                            },
+                            {
+                                "id": 5,
+                                "componentName": "Initiatives / Tactics",
+                                "color": "turqoise",
+                                "position": {"x": 52, "y": 52}
+                            },
+                            {
+                                "id": 6,
+                                "componentName": "Ideas",
+                                "color": "blue",
+                                "position": {"x": 56, "y": 56}
+                            },
+                            {
+                                "id": 7,
+                                "componentName": "Stories",
+                                "color": "purple",
+                                "position": {"x": 60, "y": 60}
+                            }
             ]
         };
     }
